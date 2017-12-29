0bf93f6a04cddf8fc3ca2592d1b9999979bac9d7
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-28 21:35:12 -0500

    Fetch data from Unsplash

--- RandomlyBeautiful/ViewControllers/ImagesViewController.swift
+++ RandomlyBeautiful/ViewControllers/ImagesViewController.swift
@@ -12,7 +12,7 @@ class ImagesViewController: UIViewController {
     @IBOutlet var activitySpinner: UIActivityIndicatorView!
     @IBOutlet var creditLabel: UILabel!
     
-    let appId = "2663fbdc32f24e9786890858bfab5ffc029602abdce2e9a64e6225612b41697e"
+    let appID = "2663fbdc32f24e9786890858bfab5ffc029602abdce2e9a64e6225612b41697e"
     var category: String?
     
     var imageViews: [UIImageView] = []
@@ -27,10 +27,21 @@ class ImagesViewController: UIViewController {
         creditLabel.alpha = 9
         creditLabel.clipsToBounds = true
         creditLabel.layer.cornerRadius = 15
+        
+        guard let url = URL(string: "https://api.unsplash.com/search/photos?client_id=\(appID)&query=\(category ?? "cats")&per_page=100") else { return }
+        
+        DispatchQueue.global(qos: .userInteractive).async {
+            self.fetch(url)
+        }
     }
     
     func fetch(_ url: URL) {
-        
+        if let data = try? Data(contentsOf: url) {
+            let json = JSON(data)
+            images = json["results"].arrayValue
+            
+            downloadImage()
+        }
     }
     
     func downloadImage() {
