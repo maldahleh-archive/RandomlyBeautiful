baa72c0b326c7c2d2e82d6fb35d361fbcacc3f72
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-28 21:20:59 -0500

    Implement base for Unsplash networking

--- RandomlyBeautiful/ViewControllers/ImagesViewController.swift
+++ RandomlyBeautiful/ViewControllers/ImagesViewController.swift
@@ -12,5 +12,14 @@ class ImagesViewController: UIViewController {
     @IBOutlet var activitySpinner: UIActivityIndicatorView!
     @IBOutlet var creditLabel: UILabel!
     
+    let appId = "2663fbdc32f24e9786890858bfab5ffc029602abdce2e9a64e6225612b41697e"
     var category: String?
+    
+    func fetch(_ url: URL) {
+        
+    }
+    
+    func downloadImage() {
+        
+    }
 }
