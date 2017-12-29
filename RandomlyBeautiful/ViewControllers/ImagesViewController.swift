eb9ffaea7079abf9d62f1d29f0c6a75c676d61e3
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-28 21:26:59 -0500

    Prepare Images VC UI when first shown

--- RandomlyBeautiful/ViewControllers/ImagesViewController.swift
+++ RandomlyBeautiful/ViewControllers/ImagesViewController.swift
@@ -15,6 +15,20 @@ class ImagesViewController: UIViewController {
     let appId = "2663fbdc32f24e9786890858bfab5ffc029602abdce2e9a64e6225612b41697e"
     var category: String?
     
+    var imageViews: [UIImageView] = []
+    var images: [JSON] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageViews = view.subviews.flatMap { $0 as? UIImageView }
+        imageViews.forEach { $0.alpha = 0 }
+        
+        creditLabel.alpha = 9
+        creditLabel.clipsToBounds = true
+        creditLabel.layer.cornerRadius = 15
+    }
+    
     func fetch(_ url: URL) {
         
     }
